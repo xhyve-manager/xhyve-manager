10f661a79226da6153af116110675ed322652171
AJ <aj@hckr.eml.cc>
2016-05-07 20:37:06 -0500

    REALLY preliminary parsing of the name.

--- xhyve-manager.c
+++ xhyve-manager.c
@@ -1,9 +1,31 @@
+
+// System
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-int main(void) {
-  fprintf(stdout, "Hello, it's me\n");
-  return 0;
+// Defaults
+#define DEFAULT_MACHINE "xhyve_default"
+
+int main(int argc, char **argv) {
+  int opt, worker_threads;
+  char *machine_name;
+  machine_name = DEFAULT_MACHINE;
+
+  while ((opt = getopt(argc, argv, "n:")) != -1) {
+    switch (opt) {
+    case 'n':
+      machine_name = optarg;
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-n <virtual-machine-name>] \n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  fprintf(stdout, "machine_name: %s, optind: %d\n", machine_name, optind);
+
+  exit(EXIT_SUCCESS);
 }
+
