d303bf63f529f50317eb4bd1bbc7b80aee9f9f13
AJ <aj@hckr.eml.cc>
2016-05-08 07:49:37 -0500

    Reorganize and use const

--- xhyve-manager.c
+++ xhyve-manager.c
@@ -14,6 +14,13 @@
 #define COMMAND 1
 #define MACHINE_NAME 2
 
+// Messages
+#define MESSAGE_RUN_COMMAND "%s machine %s"
+
+// Error Messages
+#define ERROR_INVALID_COMMAND "%s is not a valid command."
+#define ERROR_MACHINE_NOTFOUND "Could not find VM %s"
+
 // Valid Commands
 char *commands[] = {
   "list",
@@ -22,18 +29,38 @@ char *commands[] = {
   "start"
 };
 
+// Functions
+int is_valid_command(const char *command);
+void list_machines();
+void usage(const char *program_exec);
+void run_command(const char *command, const char *machine_name);
+
+int is_valid_command(const char *command) {
+  int isValid = 0;
+  char *temp = NULL;
+  temp = *commands;
+
+  return isValid;
+}
+
 void list_machines() {
   fprintf(stdout, "Here be a list of machines:\n");
   fprintf(stdout, " - default\n");
   fprintf(stdout, " - different\n");
 }
 
-void usage(char **argv) {
-  fprintf(stderr, "Usage: %s <command> <virtual-machine-name> \n", argv[PROGRAM_EXEC]);
+void usage(const char *program_exec) {
+  fprintf(stderr, "Usage: %s <command> <virtual-machine-name> \n", program_exec);
 }
 
-void run_command(char *command, char *machine_name) {
-  fprintf(stdout, "%s machine %s\n", command, machine_name);
+void run_command(const char *command, const char *machine_name) {
+  if (is_valid_command(command)) {
+    fprintf(stdout, MESSAGE_RUN_COMMAND, command, machine_name);
+    fprintf(stdout, "\n");
+  } else {
+    fprintf(stderr, ERROR_INVALID_COMMAND, command);
+    fprintf(stdout, "\n");
+  }
 }
 
 int main(int argc, char **argv) {
@@ -42,7 +69,7 @@ int main(int argc, char **argv) {
   } else if (!argv[MACHINE_NAME] && argv[COMMAND]) {
     list_machines();
   } else {
-    usage(argv);
+    usage(argv[PROGRAM_EXEC]);
     exit(EXIT_FAILURE);
   }
   exit(EXIT_SUCCESS);
