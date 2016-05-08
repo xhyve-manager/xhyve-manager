025329d6db5a4c6d40125d28e1b225dbd340b120
AJ <aj@hckr.eml.cc>
2016-05-08 10:07:34 -0500

    Add comments

--- xhyve-manager.c
+++ xhyve-manager.c
@@ -1,6 +1,11 @@
 /**
  * xhyve-manager
  * a simple CLI utility to manage xhyve virtual machines.
+ *
+ * Usage: ./xhyve-manager \
+ *   + {list} all available machines
+ *   + {create,delete,start} <machine-name>
+ *
  **/
 
 // System
