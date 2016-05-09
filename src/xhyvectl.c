1d0598d1e93accfc74dba51966b4940a3b607a12
AJ <aj@hckr.eml.cc>
2016-05-09 11:14:41 -0500

    Test ini parsing.

--- src/xhyvectl.c
+++ src/xhyvectl.c
@@ -91,6 +91,40 @@ void print_machine_info(xvirtual_machine_t machine);
 void print_machine_options(xvirtual_machine_options_t machine_options);
 void cleanup();
 
+typedef struct
+{
+  int version;
+  const char* name;
+  const char* email;
+} configuration;
+
+static int handler(void* user, const char* section, const char* name,
+                   const char* value)
+{
+  configuration* pconfig = (configuration*)user;
+
+#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+  if (MATCH("protocol", "version")) {
+    pconfig->version = atoi(value);
+  } else if (MATCH("user", "name")) {
+    pconfig->name = strdup(value);
+  } else if (MATCH("user", "email")) {
+    pconfig->email = strdup(value);
+  } else {
+    return 0;  /* unknown section/name, error */
+  }
+  return 1;
+}
+
+void test_ini() {
+  configuration config;
+
+  if (ini_parse("test.ini", handler, &config) < 0) {
+    printf("Can't load 'test.ini'\n");
+  }
+  printf("Config loaded from 'test.ini': version=%d, name=%s, email=%s\n",
+         config.version, config.name, config.email);
+}
 // Tasks
 void list_machines() {
   fprintf(stdout, "Here be a list of machines:\n");
@@ -141,6 +175,7 @@ void machine_status(const char *machine_name) {
 
 // Main
 int main(int argc, char **argv) {
+  test_ini();
   program_exec = argv[PROGRAM_EXEC];
   if (argv[COMMAND]) {
     homedir = get_homedir();
